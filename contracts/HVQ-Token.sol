2b8466ff3d27864efc874ffc627523a8ae70bd48
Ha Quoc <havuongquoc2000@gmail.com>
2022-04-09 14:20:02 +0700

    initialize project

---
+++ contracts/HVQ-Token.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract HVQToken is Ownable {
+    uint totalSupply = 1000000;
+    constructor (){
+
+    }
+}
